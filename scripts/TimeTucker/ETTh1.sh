f7235c29c58d44dce1dc7e3d3cc96c299ee5a798
yaoyaohe-cn <108442516+yaoyaohe-cn@users.noreply.github.com>
2026-06-14 20:05:28 +0800

    Create ETTh1.sh

---
+++ scripts/TimeTucker/ETTh1.sh
@@ -0,0 +1,104 @@
+model_name=TimeTucker
+root_path_name=../dataset/
+data_path_name=ETTh1.csv
+model_id_name=ETTh1
+data_name=ETTh1
+seq_len=720
+gpu=${GPU:-0}
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+if [ ! -d "./logs/${model_id_name}" ]; then
+    mkdir ./logs/${model_id_name}
+fi
+dir=./logs/${model_id_name}
+
+pred_len=96
+python -u run_longExp.py \
+    --is_training 1 \
+    --orthogonal_weight 0.16 \
+    --root_path "$root_path_name" \
+    --data_path "$data_path_name" \
+    --model_id "${model_id_name}_${seq_len}_${pred_len}" \
+    --model "$model_name" \
+    --data "$data_name" \
+    --features M \
+    --seq_len "$seq_len" \
+    --pred_len "$pred_len" \
+    --period_len 24 \
+    --enc_in 7 \
+    --train_epochs 30 \
+    --patience 5 \
+    --basis_num 6 \
+    --gpu $gpu \
+    --itr 1 \
+    --batch_size 64 \
+    --learning_rate 1e-1 > "$dir/${model_id_name}_${seq_len}_${pred_len}.log"
+
+pred_len=192
+python -u run_longExp.py \
+    --is_training 1 \
+    --orthogonal_weight 0.16 \
+    --root_path "$root_path_name" \
+    --data_path "$data_path_name" \
+    --model_id "${model_id_name}_${seq_len}_${pred_len}" \
+    --model "$model_name" \
+    --data "$data_name" \
+    --features M \
+    --seq_len "$seq_len" \
+    --pred_len "$pred_len" \
+    --period_len 24 \
+    --enc_in 7 \
+    --train_epochs 30 \
+    --patience 5 \
+    --basis_num 6 \
+    --gpu $gpu \
+    --itr 1 \
+    --batch_size 256 \
+    --learning_rate 4e-1 > "$dir/${model_id_name}_${seq_len}_${pred_len}.log"
+
+
+pred_len=336
+python -u run_longExp.py \
+    --is_training 1 \
+    --orthogonal_weight 0.08 \
+    --root_path "$root_path_name" \
+    --data_path "$data_path_name" \
+    --model_id "${model_id_name}_${seq_len}_${pred_len}" \
+    --model "$model_name" \
+    --data "$data_name" \
+    --features M \
+    --seq_len "$seq_len" \
+    --pred_len "$pred_len" \
+    --period_len 24 \
+    --enc_in 7 \
+    --train_epochs 30 \
+    --patience 5 \
+    --basis_num 6 \
+    --gpu $gpu \
+    --itr 1 \
+    --batch_size 256 \
+    --learning_rate 4e-1 > "$dir/${model_id_name}_${seq_len}_${pred_len}.log"
+
+
+pred_len=720
+python -u run_longExp.py \
+    --is_training 1 \
+    --orthogonal_weight 0.12 \
+    --root_path "$root_path_name" \
+    --data_path "$data_path_name" \
+    --model_id "${model_id_name}_${seq_len}_${pred_len}" \
+    --model "$model_name" \
+    --data "$data_name" \
+    --features M \
+    --seq_len "$seq_len" \
+    --pred_len "$pred_len" \
+    --period_len 24 \
+    --enc_in 7 \
+    --train_epochs 30 \
+    --patience 5 \
+    --basis_num 6 \
+    --gpu $gpu \
+    --itr 1 \
+    --batch_size 64 \
+    --learning_rate 5e-2 > "$dir/${model_id_name}_${seq_len}_${pred_len}.log"
